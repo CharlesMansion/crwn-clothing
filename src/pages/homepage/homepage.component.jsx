ae276c3f35d3e1687b2ef2b78774037acbb78b39
Nicolas  Nollomont <nicolasnollomont@Whatever.local>
2019-08-14 11:01:12 +0200

    adding styling and made first components for homepage

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './homepage.styles.scss';
+
+import Directory from '../../components/directory-menu/directory-menu.component';
+import './homepage.styles.scss'
+
+export const Homepage = () => {
+	return (
+		<div className="homepage">
+			<Directory/>
+		</div>
+ )
+	
+}
